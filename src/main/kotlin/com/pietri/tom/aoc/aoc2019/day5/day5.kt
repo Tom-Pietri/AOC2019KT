726d832d17eeb54ff5895f8f1289515699ba92e9
Tom Pietri <tom.pietri@4sh.fr>
2019-12-07 08:42:07 +0100

    Extracted opcode computing

--- src/main/kotlin/com/pietri/tom/aoc/aoc2019/day5/day5.kt
+++ src/main/kotlin/com/pietri/tom/aoc/aoc2019/day5/day5.kt
@@ -10,95 +10,103 @@ fun runOpCode(s: String, value: Int): Int {
     var opCode = intcodes[position] % 100;
     while (opCode != 99) {
         val firstParamMode = (intcodes[position] / 100 % 10)
-        position = when (opCode) {
-            1, 2 -> {
-                val secondParamMode = (intcodes[position] / 1000 % 10)
-                val param1 = if (firstParamMode == 0) {
-                    intcodes[intcodes[position + 1]]
-                } else {
-                    intcodes[position + 1]
-                }
-
-                val param2 = if (secondParamMode == 0) {
-                    intcodes[intcodes[position + 2]]
-                } else {
-                    intcodes[position + 2]
-                }
-
-                val result = if (opCode == 1) {
-                    param1 + param2
-                } else {
-                    param1 * param2
-                }
-
-                intcodes[intcodes[position + 3]] = result
-
-                position + 4
-            }
-            3 -> {
-                val param1 = intcodes[position + 1];
-                intcodes[param1] = value
-                position + 2
-            }
-            4 -> {
-                val param1 = intcodes[position + 1];
-                output = intcodes[param1]
-                position + 2
-            }
-            5, 6, 7, 8 -> {
-                val secondParamMode = (intcodes[position] / 1000 % 10)
-                val param1 = if (firstParamMode == 0) {
-                    intcodes[intcodes[position + 1]]
-                } else {
-                    intcodes[position + 1]
-                }
-
-                val param2 = if (secondParamMode == 0) {
-                    intcodes[intcodes[position + 2]]
-                } else {
-                    intcodes[position + 2]
-                }
-
-                when (opCode) {
-                    5 -> {
-                        if (param1 != 0) {
-                            param2;
-                        } else {
-                            position + 3;
-                        }
-                    }
-                    6 -> {
-                        if (param1 == 0) {
-                            param2;
-                        } else {
-                            position + 3;
-                        }
-                    }
-                    7 -> {
-                        if (param1 < param2) {
-                            intcodes[intcodes[position + 3]] = 1;
-                        } else {
-                            intcodes[intcodes[position + 3]] = 0
-                        }
-                        position + 4;
-                    }
-                    8 -> {
-                        if (param1 == param2) {
-                            intcodes[intcodes[position + 3]] = 1
-                        } else {
-                            intcodes[intcodes[position + 3]] = 0
-                        }
-                        position + 4
-                    }
-                    else -> -1
-                }
-            }
-            else -> -1
-        }
+        val pair = getOutputAndPositionForOpcode(opCode, intcodes, position, firstParamMode, value, output)
+        output = pair.second
+        position = pair.first
         opCode = intcodes[position] % 100
     }
 
     return output;
+}
+
+private fun getOutputAndPositionForOpcode(opCode: Int, intcodes: MutableList<Int>, position: Int, firstParamMode: Int, value: Int, output: Int): Pair<Int, Int> {
+    var opCodeOutput = output
+    val nextPosition = when (opCode) {
+        1, 2 -> {
+            val secondParamMode = (intcodes[position] / 1000 % 10)
+            val param1 = if (firstParamMode == 0) {
+                intcodes[intcodes[position + 1]]
+            } else {
+                intcodes[position + 1]
+            }
+
+            val param2 = if (secondParamMode == 0) {
+                intcodes[intcodes[position + 2]]
+            } else {
+                intcodes[position + 2]
+            }
+
+            val result = if (opCode == 1) {
+                param1 + param2
+            } else {
+                param1 * param2
+            }
+
+            intcodes[intcodes[position + 3]] = result
+
+            position + 4
+        }
+        3 -> {
+            val param1 = intcodes[position + 1];
+            intcodes[param1] = value
+            position + 2
+        }
+        4 -> {
+            val param1 = intcodes[position + 1];
+            opCodeOutput = intcodes[param1]
+            position + 2
+        }
+        5, 6, 7, 8 -> {
+            val secondParamMode = (intcodes[position] / 1000 % 10)
+            val param1 = if (firstParamMode == 0) {
+                intcodes[intcodes[position + 1]]
+            } else {
+                intcodes[position + 1]
+            }
+
+            val param2 = if (secondParamMode == 0) {
+                intcodes[intcodes[position + 2]]
+            } else {
+                intcodes[position + 2]
+            }
+
+            when (opCode) {
+                5 -> {
+                    if (param1 != 0) {
+                        param2;
+                    } else {
+                        position + 3;
+                    }
+                }
+                6 -> {
+                    if (param1 == 0) {
+                        param2;
+                    } else {
+                        position + 3;
+                    }
+                }
+                7 -> {
+                    if (param1 < param2) {
+                        intcodes[intcodes[position + 3]] = 1;
+                    } else {
+                        intcodes[intcodes[position + 3]] = 0
+                    }
+                    position + 4;
+                }
+                8 -> {
+                    if (param1 == param2) {
+                        intcodes[intcodes[position + 3]] = 1
+                    } else {
+                        intcodes[intcodes[position + 3]] = 0
+                    }
+                    position + 4
+                }
+                else -> -1
+            }
+        }
+        else -> -1
+    }
+    return Pair(nextPosition, opCodeOutput)
 }
 
 fun main() {
