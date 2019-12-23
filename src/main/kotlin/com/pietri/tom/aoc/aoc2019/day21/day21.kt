15c527d12cd23f28b4415ed816321840ea76b5c7
Tom Pietri <tom.pietri@4sh.fr>
2019-12-23 09:15:15 +0100

    Solution for day 21 part 2

--- src/main/kotlin/com/pietri/tom/aoc/aoc2019/day21/day21.kt
+++ src/main/kotlin/com/pietri/tom/aoc/aoc2019/day21/day21.kt
@@ -17,6 +17,36 @@ fun computeFirstSolution(intcodeProgram: String): BigInteger {
     return outputs.last()
 }
 
+fun computeSecondSolution(intcodeProgram: String): BigInteger {
+
+    val asciiProgram = listOf(
+            "NOT A T",
+            "NOT B J",
+            "OR T J",
+            "NOT C T",
+            "OR T J",
+            "AND D J",
+            "NOT E T",
+            "NOT T T",
+            "OR H T",
+            "AND T J",
+            "RUN")
+    val computer = Computer(intcodeProgram.split(",").map { it.toBigInteger() }.toMutableList())
+    val outputs = computer.getNexOutput(asciiProgramToIntcodeInput(asciiProgram).toMutableList())
+    return outputs.last()
+}
+
+fun printOutputs(outputs: List<BigInteger>) {
+    outputs.forEach {
+        when (it) {
+            BigInteger.valueOf(35) -> print('#')
+            BigInteger.valueOf(46) -> print('.')
+            BigInteger.valueOf(10) -> println()
+            else -> println(it)
+        }
+    }
+}
+
 fun asciiProgramToIntcodeInput(asciiProgram: List<String>): List<Int> {
 
     return asciiProgram.map { it.map(Char::toInt) }
--- src/test/kotlin/com/pietri/tom/aoc/aoc2019/day21/Day21KtTest.kt
+++ src/test/kotlin/com/pietri/tom/aoc/aoc2019/day21/Day21KtTest.kt
@@ -7,8 +7,14 @@ import java.math.BigInteger
 
 internal class Day21KtTest {
     @Test
-    fun test() {
+    fun firstSolution() {
         val input = File("./src/main/kotlin/com/pietri/tom/aoc/aoc2019/day21/day21.input").readLines(Charsets.UTF_8).get(0)
         assertThat(computeFirstSolution(input)).isEqualTo(BigInteger.valueOf(19350375))
+    }
+
+    @Test
+    fun secondSolution() {
+        val input = File("./src/main/kotlin/com/pietri/tom/aoc/aoc2019/day21/day21.input").readLines(Charsets.UTF_8).get(0)
+        assertThat(computeSecondSolution(input)).isEqualTo(BigInteger.valueOf(1143990055))
     }
 }
